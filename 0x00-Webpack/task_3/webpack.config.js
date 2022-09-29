bfb16e4dba4251c2694be4dcc131e105f6cc26ba
Tres Serio <treserio@hotmail.com>
2022-09-28 20:02:05 -0500

    this project is bad

--- 0x00-Webpack/task_3/webpack.config.js
+++ 0x00-Webpack/task_3/webpack.config.js
@@ -1,73 +1,55 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
-const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = {
   mode: 'development',
   entry: {
-    // Note: entry paths are relative
-    header: './modules/header/header.js',
-    body: './modules/body/body.js',
-    footer: './modules/footer/footer.js',
+    all: [
+      './modules/header/header.js',
+      './modules/body/body.js',
+      './modules/footer/footer.js',
+    ]
   },
   output: {
-    // Note: output paths are absolute
-    path: path.resolve(__dirname, 'public'),
-    // Maps output to name key file in entry
     filename: '[name].bundle.js',
-    assetModuleFilename: '[name][ext]',
+    path: path.resolve(__dirname, 'public')
   },
   module: {
     rules: [
       {
         test: /\.css$/,
-        // Rather than injecting like file-loader, adds scripts to index.html
-        use: [ MiniCssExtractPlugin.loader, 'css-loader' ],
+        use: ['style-loader', 'css-loader']
       },
       {
-        test: /\.(jpe?g|png|gif|svg)$/i,
+        test: /\.(png|jpe?g|gif)$/i,
         use: [
+          'file-loader',
           {
             loader: 'image-webpack-loader',
             options: {
-              limit: 8192,
-            }
+              bypassOnDebug: true,
+              disable: true
+            },
           },
         ],
       },
-    ],
+    ]
   },
-  // Starts dev server on port 8564 (in development mode)
+  devtool: 'inline-source-map',
   devServer: {
-    static: path.join(__dirname, 'public'),
+    static: path.join(__dirname, './public'),
     compress: true,
-    port: 8564,
+    port: 8564
   },
   plugins: [
-    // Automatically creates index.html file
-    new HtmlWebpackPlugin(),
-    // Cleans folder before building
-    new CleanWebpackPlugin(),
-    // Plugin to minify css
-    new MiniCssExtractPlugin()
+    new CleanWebpackPlugin(), // clean the public folder before building
+    new HtmlWebpackPlugin() // Generates default index.html
   ],
-  // Generate source map (this option is slow for publishing one file but maintains quality)
-  devtool: 'inline-source-map',
-  // Optimize by splitting module into smaller chunks
   optimization: {
     splitChunks: {
-      chunks: 'all',
-    },
-    minimizer: [
-      // Minimize CSS files
-      new CssMinimizerPlugin(),
-      // Minimize JS files - don't typically have to, but CSS minifying overrides
-      new TerserPlugin(),
-    ],
-    // Above minimizing is for production - this is for devServer
-    minimize: true,
-  },
+      chunks: 'all' // split all chunks
+    }
+  }
+
 };
