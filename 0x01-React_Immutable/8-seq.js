a037373144aceafc162bce43df4ecb58a931e83b
Tres Serio <treserio@hotmail.com>
2022-10-19 17:19:48 -0500

    8.1 linter

--- 0x01-React_Immutable/8-seq.js
+++ 0x01-React_Immutable/8-seq.js
@@ -1,16 +1,18 @@
 import Immutable from 'immutable';
 
 export default function printBestStudents(grades) {
-  function upperFirst(word) {
-    return word.charAt(0).toUpperCase() + word.slice(1)
+  function firstUpper(word) {
+    return word.charAt(0).toUpperCase() + word.slice(1);
   }
 
   const gradeSeq = Immutable.Seq(grades)
-    .filter(grade => grade.score >= 70)
+    .filter((grade) => grade.score >= 70)
     .map((grade) => {
-      grade.firstName = upperFirst(grade.firstName);
-      grade.lastName = upperFirst(grade.lastName);
-      return grade
-    })
-  console.log(gradeSeq.toJS())
+      // eslint doesn't like reassignment to grade.firstName
+      const thing = grade;
+      thing.firstName = firstUpper(grade.firstName);
+      thing.lastName = firstUpper(grade.lastName);
+      return thing;
+    });
+  console.log(gradeSeq.toJS());
 }
