a91133d4fb3de5534674c21e6161964765065998
Tres Serio <treserio@hotmail.com>
2022-10-19 11:27:21 -0500

    4 init

--- 0x01-React_Immutable/3-list.js
+++ 0x01-React_Immutable/3-list.js
@@ -1,11 +1,11 @@
 import Immutable from 'immutable';
 
 function getListObject(arr) {
-  Immutable.List(arr);
+  return Immutable.List(arr);
 }
 
 function addElementToList(list, element) {
-  list.push(element);
+  return list.push(element);
 }
 
 export default { getListObject, addElementToList };
