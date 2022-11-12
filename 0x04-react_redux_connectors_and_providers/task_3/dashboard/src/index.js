9993abfdaece74a76ad83d74d935bc53c4207d24
Tres Serio <treserio@hotmail.com>
2022-11-12 09:15:22 -0600

    +devtool extension

--- 0x04-react_redux_connectors_and_providers/task_3/dashboard/src/index.js
+++ 0x04-react_redux_connectors_and_providers/task_3/dashboard/src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import { ReduxApp } from './App/App';
 
+window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+
 ReactDOM.render(
   <React.StrictMode>
     <ReduxApp />
